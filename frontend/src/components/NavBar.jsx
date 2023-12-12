79f65efbedc18a30ce1076d0ad4dfb68c5be007b
Vinc441 <vincentlouvart82@gmail.com>
2023-12-12 11:25:52 +0100

    mise en place navBar et sass

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-
   function determineActive({ isActive }) {
     if (isActive === true) {
       return "active";
@@ -10,23 +9,23 @@ function NavBar() {
     }
   }
 
-
-  return <div className="navBar">
-    <div className="Int-navBar">
-<nav className="nav-pt-1">
-  <NavLink to={"/"} className={determineActive}>HOME</NavLink>
-  <NavLink to={"/Contact"}> CONTACT</NavLink>
-</nav>
-<img src="./Images/logo.png" alt="logo-arcade"/>
-<nav className="nav-pt-2">
-<p>INSCRIPTION</p>
-<p>CONNEXION</p>
-
-</nav>
-
-
-</div>
-    </div>;
+  return (
+    <div className="navBar">
+      <div className="Int-navBar">
+        <nav className="nav-pt-1">
+          <NavLink to={"/"} className={determineActive}>
+            HOME
+          </NavLink>
+          <NavLink to={"/Contact"}> CONTACT</NavLink>
+        </nav>
+        <img src="./Images/logo.png" alt="logo-arcade" />
+        <nav className="nav-pt-2">
+          <p>INSCRIPTION</p>
+          <p>CONNEXION</p>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
