34577bc948fc4afc6f8b1aa07350aa9760c04ac4
Karim Rejani <karim.rjn02@gmail.com>
2023-12-12 11:44:03 +0100

    structuring classement/lots page

--- frontend/src/pages/Classement.jsx
+++ frontend/src/pages/Classement.jsx
@@ -1,5 +1,22 @@
 function Classement() {
-  return <div>Classement</div>;
+  return (
+    <div className="parent-container">
+      <div className="classement">
+        <h1>Classement de la semaine</h1>
+        <img
+          className="podium-img"
+          src="/Images/Podium/podiumvector.png"
+          alt="podium"
+        />
+      </div>
+      <div className="récompenses">
+        <h2>Récompenses</h2>
+      </div>
+      <div className="lots">
+        <h2>Lots à gagner</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Classement;
