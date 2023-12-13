433000a05887242a60b651632edcf683fb03cd02
Karim Rejani <karim.rjn02@gmail.com>
2023-12-13 15:36:12 +0100

    fixing conflict

--- backend/src/router.js
+++ backend/src/router.js
@@ -34,19 +34,19 @@ router.post("/score", scoreControllers.add);
 router.post("/lot", lotControllers.add);
 router.post("/utilisateur", utilisateurControllers.add);
 
-// Route to modify an item
-router.put("/evenement/:id", evenementControllers.update);
-router.put("/jeu/:id", jeuxControllers.update);
-router.put("/score/:id", scoreControllers.update);
-router.put("/lot/:id", lotControllers.update);
-router.put("/utilisateur/:id", utilisateurControllers.update);
+// // Route to modify an item
+// router.put("/evenement/:id", evenementControllers.update);
+// router.put("/jeu/:id", jeuxControllers.update);
+// router.put("/score/:id", scoreControllers.update);
+// router.put("/lot/:id", lotControllers.update);
+// router.put("/utilisateur/:id", utilisateurControllers.update);
 
-// Route to delete an item
-router.delete("/evenement/:id", evenementControllers.delete);
-router.delete("/jeu/:id", jeuxControllers.delete);
-router.delete("/score/:id", scoreControllers.delete);
-router.delete("/lot/:id", lotControllers.delete);
-router.delete("/utilisateur/:id", utilisateurControllers.delete);
+// // Route to delete an item
+// router.delete("/evenement/:id", evenementControllers.delete);
+// router.delete("/jeu/:id", jeuxControllers.delete);
+// router.delete("/score/:id", scoreControllers.delete);
+// router.delete("/lot/:id", lotControllers.delete);
+// router.delete("/utilisateur/:id", utilisateurControllers.delete);
 
 /* ************************************************************************* */
 
