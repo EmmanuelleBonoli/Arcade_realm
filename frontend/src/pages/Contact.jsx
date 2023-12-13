dbda400b16a989ce683c2273a1d662f659bf3921
Alexandra <alexandradepourtoux93@gmai.com>
2023-12-13 12:00:07 +0100

    ajout du formulaire contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -2,31 +2,42 @@ function Contact() {
   return (
     <div>
       <div className="container">
-      <div className="container-form">
-        <div className="form-contact">
-          <div className="header-contact">
-            <img src="./images/Contact/PacmanContact.png" alt="logo-contact" className="pacman" />
-          <h1>Contact</h1>
+        <div className="container-form">
+          <div className="form-contact">
+            <div className="header-contact">
+              <img
+                src="./images/Contact/PacmanContact.png"
+                alt="logo-contact"
+                className="pacman"
+              />
+              <h1>Contact</h1>
+            </div>
+            <div className="input-container">
+              <p>Nom, Prénom</p>
+              <input type="text" placeholder="" className="name" />
+              <p>E-mail</p>
+              <input type="text" placeholder="" className="email" />
+              <p>Message</p>
+              <input
+                type="text"
+                placeholder="Entrez votre message"
+                className="message"
+              />
+            </div>
+            <button type="submit" className="btn-contact">
+              Envoyer
+            </button>
+<div className="adresse">
+            <p>
+              Nous retrouver:<br></br> 4 rue Baron, 44000 Nantes
+            </p>
+            </div>
+            <div className="reseaux-sociaux">
+            <p>Nos réseaux :</p>
+            </div>
           </div>
-          <div className="input-container">
-          <p>Nom, Prénom</p>
-          <input type="text" placeholder="" />
-          <p>E-mail</p>
-          <input type="text" placeholder="" />
-          <p>Message</p>
-          <input type="text" placeholder="" />
-          </div>
-          <button type="submit" className="btn-contact">
-            Envoyer
-          </button>
-          <p>Nous retrouver:<br></br> 4 rue Baron,
-          44000 Nantes</p>
-
-          <p>Nos réseaux :</p>
-
         </div>
       </div>
-    </div>
     </div>
   );
 }
