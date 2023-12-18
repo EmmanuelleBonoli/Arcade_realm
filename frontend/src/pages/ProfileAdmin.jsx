f92ae6e1bd90939ef5e78b66c34f9be5e0426873
Alexandra <alexandradepourtoux93@gmai.com>
2023-12-18 16:25:19 +0100

    création du composant profile admin

---
+++ frontend/src/pages/ProfileAdmin.jsx
@@ -0,0 +1,21 @@
+function ProfileAdmin() {
+    return (
+      <div className="profileAdmin">
+        <div className="adminLayout">
+          <div className="buttonsChoice">
+            <button type="button" className="">
+              Données Personnelles
+            </button>
+            <button type="button" className="">
+              Gestion des services
+            </button>
+            <button type="button" className="">
+              Gestion des profils
+            </button>
+          </div>
+          <div className="displayChoice" />
+        </div>
+      </div>
+    );
+  }
+  export default ProfileAdmin;
