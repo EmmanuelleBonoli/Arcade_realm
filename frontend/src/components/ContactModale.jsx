318bf558c2843318ebb6ecb575599379f93e4f12
Alexandra <alexandradepourtoux93@gmai.com>
2023-12-21 10:16:25 +0100

    création de la modale pour la page contact

---
+++ frontend/src/components/ContactModale.jsx
@@ -0,0 +1,15 @@
+export default function ContactModale() {
+  return (
+    <div>
+      <div className="container-modale">
+        <div className="submit-modale">
+          <p>
+            Merci d'avoir contacté l'équipe <span>Arcade Realm</span> !
+          </p>
+          <p> Nous reviendrons vers vous rapidement.</p>
+          <img src="/images/Contact/mariobye.png" alt="mariobye" />
+        </div>
+      </div>
+    </div>
+  );
+}
