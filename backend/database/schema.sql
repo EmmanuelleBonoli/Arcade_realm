692c9f66dd96d429761585b1a04d13499c0f039e
Alexandra <alexandradepourtoux93@gmai.com>
2024-01-23 15:52:44 +0100

    pull

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -5,275 +5,109 @@ CREATE DATABASE Arcade;
 
 USE Arcade;
 
-CREATE TABLE
-    utilisateur (
-        id INT primary key auto_increment NOT NULL,
-        pseudo VARCHAR(80) NOT NULL,
-        email VARCHAR(80) NOT NULL,
-        hashed_password VARCHAR(250) NOT NULL,
-        image VARCHAR(250) NULL,
-        admin BOOLEAN NOT NULL DEFAULT FALSE,
-        points INT NOT NULL DEFAULT FALSE
-    );
+CREATE TABLE utilisateur (
+    id INT primary key auto_increment NOT NULL, pseudo VARCHAR(80) NOT NULL, email VARCHAR(80) NOT NULL, hashed_password VARCHAR(250) NOT NULL, image VARCHAR(250) NOT NULL DEFAULT '', admin BOOLEAN NOT NULL DEFAULT FALSE, points INT NOT NULL DEFAULT FALSE, podium BOOLEAN NOT NULL DEFAULT FALSE, tickets INT NOT NULL DEFAULT FALSE
+);
 
-CREATE TABLE
-    jeu (
-        id INT primary key auto_increment NOT NULL,
-        name varchar(255) NOT NULL,
-        image VARCHAR(255) NOT NULL,
-        regles TEXT NOT NULL,
-        actif BOOLEAN NOT NULL,
-        physique BOOLEAN NOT NULL,
-        date DATE NOT NULL,
-        nb_borne INT NOT NULL,
-        description TEXT NOT NULL
-    );
+CREATE TABLE jeu (
+    id INT primary key auto_increment NOT NULL, name varchar(255) NOT NULL, image VARCHAR(255) NOT NULL, regles TEXT NOT NULL, actif BOOLEAN NOT NULL, physique BOOLEAN NOT NULL, date DATE NOT NULL, nb_borne INT NOT NULL, description TEXT NOT NULL
+);
 
-CREATE TABLE
-    lot (
-        id INT PRIMARY KEY auto_increment NOT NULL,
-        name VARCHAR(80) NOT NULL,
-        image VARCHAR(250) NOT NULL,
-        description VARCHAR(400),
-        utilisateur_id INT DEFAULT NULL,
-        win BOOLEAN NOT NULL DEFAULT FALSE,
-        exchange BOOLEAN NOT NULL DEFAULT FALSE,
-        CONSTRAINT fk_lot_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id) ON DELETE CASCADE ON UPDATE NO ACTION
-    );
+CREATE TABLE lot (
+    id INT PRIMARY KEY auto_increment NOT NULL, name VARCHAR(80) NOT NULL, image VARCHAR(250) NOT NULL, description VARCHAR(400), utilisateur_id INT DEFAULT NULL, win BOOLEAN NOT NULL DEFAULT FALSE, exchange BOOLEAN NOT NULL DEFAULT FALSE, podium INT NOT NULL DEFAULT FALSE, CONSTRAINT fk_lot_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id) ON DELETE CASCADE ON UPDATE NO ACTION
+);
 
-CREATE TABLE
-    evenement (
-        id int primary key auto_increment not null,
-        image varchar(250) not null
-    );
+CREATE TABLE evenement (
+    id int primary key auto_increment not null, image varchar(250) not null
+);
 
-CREATE TABLE
-    score (
-        id INT PRIMARY KEY auto_increment NOT NULL,
-        utilisateur_id INT NOT NULL,
-        jeu_id INT NOT NULL,
-        points INT NOT NULL,
-        CONSTRAINT fk_score_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id) ON DELETE CASCADE ON UPDATE NO ACTION,
-        CONSTRAINT fk_score_jeu FOREIGN KEY (jeu_id) REFERENCES jeu(id) ON DELETE CASCADE ON UPDATE NO ACTION
-    );
+CREATE TABLE score (
+    id INT PRIMARY KEY auto_increment NOT NULL, utilisateur_id INT NOT NULL, jeu_id INT NOT NULL, points INT NOT NULL, CONSTRAINT fk_score_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id) ON DELETE CASCADE ON UPDATE NO ACTION, CONSTRAINT fk_score_jeu FOREIGN KEY (jeu_id) REFERENCES jeu (id) ON DELETE CASCADE ON UPDATE NO ACTION
+);
 
-CREATE TABLE
-    favoris (
-        utilisateur_id INT NOT NULL,
-        jeu_id INT NOT NULL,
-        favori BOOLEAN NOT NULL DEFAULT FALSE,
-        CONSTRAINT fk_favoris_jeu FOREIGN KEY (jeu_id) REFERENCES jeu(id) ON DELETE CASCADE ON UPDATE NO ACTION,
-        CONSTRAINT fk_favoris_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id) ON DELETE CASCADE ON UPDATE NO ACTION
-    );
+CREATE TABLE favoris (
+    utilisateur_id INT NOT NULL, jeu_id INT NOT NULL, favori BOOLEAN NOT NULL DEFAULT FALSE, CONSTRAINT fk_favoris_jeu FOREIGN KEY (jeu_id) REFERENCES jeu (id) ON DELETE CASCADE ON UPDATE NO ACTION, CONSTRAINT fk_favoris_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id) ON DELETE CASCADE ON UPDATE NO ACTION
+);
 
 INSERT INTO
     utilisateur (
-        pseudo,
-        email,
-        hashed_password,
-        image,
-        admin,
-        points
+        pseudo, email, hashed_password, image, admin, points, podium, tickets
     )
 VALUES (
-        "Wild_Gamer",
-        "wildgamer@gmail.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/CrashBandicoot.png",
-        0,
-        50000,
-        1,
-        10
-    ), (
-        "Pixel_Queen",
-        "pixelqueen@gmail.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Rondoudou.png",
-        0,
-        30000,
-        2,
-        5
-    ), (
-        "Admin_Realm",
-        "adminrealm@gmail.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Ghost.png",
-        1,
-        0
-    ), (
-        "Arcade_Master",
-        "arcademaster@example.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/DonkeyKong.png",
-        0,
-        80000,
-        3,
-        0
-    ), (
-        "Joystick_Master",
-        "joystick@email.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Avatar.png",
-        0,
-        3000
-    ), (
-        "LevelUp_Legend",
-        "levelup@email.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Mario.png",
-        0,
-        10000,
-        4,
-        5
-    ), (
-        "Quest_Seeker",
-        "quest@email.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Avatar.png",
-        0,
-        4000,
-        5,
-        5
-    ), (
-        "GameOn_Guru",
-        "gameon@email.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Avatar.png",
-        0,
-        9000,
-        6,
-        5
-    ), (
-        "Pixel_Pioneer",
-        "pioneer@email.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Avatar.png",
-        0,
-        6000
-    ), (
-        "HighScore_Hero",
-        "highscore@email.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Avatar.png",
-        0,
-        1000,
-        0,
-        0
-    ), (
-        "Arcade_Adventurer",
-        "adventurer@email.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Avatar.png",
-        0,
-        2000
-    ), (
-        "Game_Champion",
-        "champion@email.com",
-        "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U",
-        "/images/Avatar/Avatar.png",
-        0,
-        5000
-    );
-
-INSERT INTO
-    lot (
-        name,
-        image,
-        description,
-        utilisateur_id,
-        win,
-        exchange
-    )
-VALUES (
-        "5x Tickets Gratuits",
-        "/images/Lots/ticketgratuit.png",
-        "5 tickets gratuits pour jouer à n'importe quel jeu de votre choix dans notre salle d'arcade !",
-        4,
-        1,
-        0
+        "Wild_Gamer", "wildgamer@gmail.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/CrashBandicoot.png", 0, 50000, 1, 10
     ),
+    (
+        "Pixel_Queen", "pixelqueen@gmail.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Rondoudou.png", 0, 30000, 2, 5
+    ),
+    (
+        "Admin_Realm", "adminrealm@gmail.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Ghost.png", 1, 0, 0, 0
+    ),
+    (
+        "Arcade_Master", "arcademaster@example.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/DonkeyKong.png", 0, 80000, 3, 0
+    ),
+    (
+        "Joystick_Master", "joystick@email.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Avatar.png", 0, 3000, 0, 10
+    ),
+    (
+        "LevelUp_Legend", "levelup@email.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Mario.png", 0, 10000, 4, 5
+    ),
+    (
+        "Quest_Seeker", "quest@email.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Avatar.png", 0, 4000, 5, 5
+    ),
+    (
+        "GameOn_Guru", "gameon@email.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Avatar.png", 0, 9000, 6, 5
+    ),
+    (
+        "Pixel_Pioneer", "pioneer@email.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Avatar.png", 0, 6000, 0, 0
+    ),
+    (
+        "HighScore_Hero", "highscore@email.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Avatar.png", 0, 1000, 0, 0
+    ),
+    (
+        "Arcade_Adventurer", "adventurer@email.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Avatar.png", 0, 2000, 0, 0
+    ),
+    (
+        "Game_Champion", "champion@email.com", "$argon2id$v=19$m=19456,t=2,p=1$NCeUqNGKyATb2N9exMVT0Q$JGUoiwb2VY2T08gka2mwdLa8vBV/B3AKgDLGxDIPt3U", "/images/Avatar/Avatar.png", 0, 5000, 0, 0
+    );
+
+INSERT INTO
+    lot (
+        name, image, description, utilisateur_id, win, exchange, podium
+    )
+VALUES
 
 (
-    "Mug Space Invaders",
-    "/images/Lots/InvadersMug.png",
-    "Égayez vos pauses café avec ce mug rétro arborant les emblématiques extraterrestres pixélisés du célèbre jeu vidéo",
-    4,
-    1,
-    0
-), (
-    "Peluche Pac-Man",
-    "/images/Lots/PeluchePacman.png",
-    "Adoptez ce compagnon doux et nostalgique inspiré du jeu d'arcade classique, idéal pour les fans de rétro gaming!",
-    1,
-    1,
-    0
-), (
-    "Mini Arcade",
-    "/images/Lots/MiniArcade.png",
-    "blabla2",
-    2,
-    2,
-    1,
-    0
-), (
-    "Monopoly Pacman",
-    "/images/Lots/MonopolyPacman.png",
-    "blabla",
-    2,
-    1,
-    0
-), (
-    "Super Nes",
-    "/images/Lots/SuperNes.png",
-    "blabla3",
-    1,
-    1,
-    0,
-    1
-), (
-    "Tasse Mario",
-    "/images/Lots/TasseMario.png",
-    "blabla3",
-    2,
-    1,
-    1
-), (
-    "Statue Zelda",
-    "/images/Lots/TriforceZelda.png",
-    "blabla3",
-    4,
-    1,
-    0
-), (
-    "Mug Lego",
-    "/images/Lots/mugLego.webp",
-    "blabla3",
-    1,
-    1,
-    1,
-    0
-), (
-    "Switch",
-    "/images/Lots/Switch.png",
-    "blabla3",
-    null,
-    0,
-    0,
-    0
-), (
-    "Déco Mario",
-    "/images/Lots/decoMario.jpg",
-    "blabla3",
-    null,
-    0,
-    0
-), (
-    "Lampe Batman",
-    "/images/Lots/LampeBatman.jpg",
-    "blabla3",
-    null,
-    0,
-    0
+    "Mug Space Invaders", "/images/Lots/InvadersMug.png", "Égayez vos pauses café avec ce mug rétro arborant les emblématiques extraterrestres pixélisés du célèbre jeu vidéo", 4, 1, 0, 0
+),
+(
+    "Peluche Pac-Man", "/images/Lots/PeluchePacman.png", "Adoptez ce compagnon doux et nostalgique inspiré du jeu d'arcade classique, idéal pour les fans de rétro gaming!", 1, 1, 0, 0
+),
+(
+    "Mini Arcade", "/images/Lots/MiniArcade.png", "blabla2", 2, 1, 0, 2
+),
+(
+    "Monopoly Pacman", "/images/Lots/MonopolyPacman.png", "blabla", 2, 1, 0, 0
+),
+(
+    "Super Nes", "/images/Lots/SuperNes.png", "blabla3", 1, 1, 0, 1
+),
+(
+    "Tasse Mario", "/images/Lots/TasseMario.png", "blabla3", 2, 1, 1, 0
+),
+(
+    "Statue Zelda", "/images/Lots/TriforceZelda.png", "blabla3", 4, 1, 0, 3
+),
+(
+    "Mug Lego", "/images/Lots/mugLego.webp", "blabla3", 1, 1, 1, 0
+),
+(
+    "Switch", "/images/Lots/Switch.png", "blabla3", null, 0, 0, 0
+),
+(
+    "Déco Mario", "/images/Lots/decoMario.jpg", "blabla3", null, 0, 0, 0
+),
+(
+    "Lampe Batman", "/images/Lots/LampeBatman.jpg", "blabla3", null, 0, 0, 0
 );
 
 INSERT INTO
@@ -290,37 +124,37 @@ INSERT INTO
 VALUES (
         "Metal Slug",
         "/images/jeux/mslug.jpg",
-        "blabla regles",
+        "Tu avances, tires sur les ennemis, évites les attaques, ramasses des bonus et sauves des otages pour avancer dans des niveaux pleins d'action.",
         0,
         1,
-        "2018-07-12",
+        "1996-04-19",
         1,
-        "blabla description"
+        "Metal Slug est un jeu de tir à défilement latéral bourré d'action, où les joueurs contrôlent des soldats armés jusqu'aux dents pour affronter des hordes d'ennemis dans des niveaux frénétiques. Avec son style rétro, ses graphismes détaillés et son gameplay rapide, c'est un classique intemporel des jeux d'arcade"
     ), (
         "Super Mario Classic",
         "/images/jeux/mario.png",
         "blabla regles",
         0,
         1,
-        "2018-07-12",
+        "1985-09-13",
         1,
-        "blabla description"
+        "Super Mario Classic est un jeu emblématique de plateforme où les joueurs incarnent Mario, sautant et courant à travers des mondes colorés pour sauver la princesse Peach des griffes de Bowser. Avec ses niveaux ingénieux, ses power-ups emblématiques et son gameplay captivant, c'est un pilier de l'histoire des jeux vidéo."
     ), (
         "Defender",
         "/images/jeux/defender.jpg",
         "blabla regles",
         0,
         1,
-        "2018-07-12",
+        "1980-02-01",
         1,
-        "blabla description"
+        "Defender est un jeu de tir spatial classique où les joueurs pilotent un vaisseau spatial, défendant des humains contre des vagues d'extraterrestres dans un environnement à défilement horizontal. Sorti dans les années 80, il est réputé pour sa difficulté élevée, son action rapide et ses mécaniques novatrices pour l'époque."
     ), (
         "Guitar Hero",
         "/images/jeux/guitare_hero.jpg",
         "Contrairement aux jeux musicaux plus traditionnels, dans Guitar Hero, votre objectif est de jouer des morceaux de musique en appuyant sur les boutons correspondants à la guitare. Suivez le rythme de la musique et appuyez sur les bonnes touches pour marquer des points. Plus vous jouez avec précision, plus votre public applaudit et votre performance s'améliore !",
         1,
         1,
-        "2005-08-12",
+        "2005-11-01",
         4,
         "Guitar Hero est une série emblématique de jeux musicaux. Annoncé lors d'un événement spécial, il a été lancé mondialement à la date de sortie. Plongez dans le monde de la musique, défiez vos amis en mode multijoueur, et devenez une rockstar virtuelle !"
     ), (
@@ -329,18 +163,19 @@ VALUES (
         "blabla regles",
         0,
         1,
-        "2018-07-12",
+        "1995-12-20",
         1,
-        "blabla description"
+        "Time Crisis est une série de jeux de tir sur rail en arcade où les joueurs utilisent un pistolet pour éliminer les ennemis tout en prenant des couvertures pour éviter les tirs adverses. Son concept distinctif de gameplay à couverture dynamique, son action intense et ses phases de temps limité en font un jeu de tir apprécié des amateurs d'arcade."
     ), (
         "Sonic Racing",
         "/images/jeux/sonic.jpg",
         "blabla regles",
         0,
         1,
-        "2018-07-12",
+        "1997-10-18",
         1,
         "Sonic Racing est une série de jeux de course mettant en vedette les personnages emblématiques de l'univers Sonic. Les joueurs participent à des courses effrénées, utilisant des power-ups et des compétences uniques pour atteindre la ligne d'arrivée en tête. Avec ses circuits variés, ses modes de jeu divertissants et son hommage à l'univers Sonic, c'est une expérience de course dynamique pour les fans de la franchise."
+
 
 ), (
     "Cars",
@@ -470,12 +305,15 @@ VALUES (
     "Galaga est un jeu de tir spatial classique où les joueurs contrôlent un vaisseau spatial pour affronter des vagues d'aliens en mouvement dans l'espace. Sorti dans les années 80, il propose un gameplay addictif où les joueurs doivent esquiver les tirs ennemis tout en éliminant les envahisseurs extraterrestres pour obtenir le meilleur score possible. Galaga est connu pour sa difficulté progressive et son style emblématique des jeux d'arcade de cette époque."
 );
 
-INSERT INTO evenement (image)
+INSERT INTO
+    evenement (image)
 VALUES (
         "/images/Evenements/affiche1.png"
-    ), (
+    ),
+    (
         "/images/Evenements/affiche2.png"
-    ), (
+    ),
+    (
         "/images/Evenements/affiche3.png"
     );
 
@@ -483,5 +321,24 @@ VALUES (
 --     favoris (utilisateur_id, jeu_id, favori)
 
 INSERT INTO
-    score (utilisateur_id, jeu_id, points)
-VALUES (1, 2, 3000), (2, 2, 2000), (3, 2, 2000), (1, 1, 5000), (2, 1, 10000), (1, 4, 50), (3, 4, 4000), (2, 4, 5000), (10, 4, 9500), (5, 9, 2000), (3, 9, 899), (8, 9, 400), (4, 15, 450), (1, 15, 3000), (9, 15, 2700), (2, 17, 3240), (6, 17, 5700), (4, 17, 1200);
+    score (
+        utilisateur_id, jeu_id, points
+    )
+VALUES (1, 2, 3000),
+    (2, 2, 2000),
+    (3, 2, 2000),
+    (1, 1, 5000),
+    (2, 1, 10000),
+    (1, 4, 50),
+    (3, 4, 4000),
+    (8, 4, 5500),
+    (10, 4, 9500),
+    (5, 9, 2000),
+    (3, 9, 899),
+    (8, 9, 400),
+    (4, 15, 450),
+    (1, 15, 3000),
+    (9, 15, 2700),
+    (2, 17, 3240),
+    (6, 17, 5700),
+    (4, 17, 1200);
